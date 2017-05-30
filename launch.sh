3c7d05edff1f7107e7cc856f1f3c1c219bf0e788
mraaaaaw <golubibolubi@gmail.com>
2017-05-30 23:06:33 +0300

    Add files via upload
    remove others

--- launch.sh
+++ launch.sh
@@ -4,9 +4,7 @@ make_template() {
 	find . -name "*.lua" | sort |
 		xgettext --from-code=utf-8 \
 			--add-comments=TRANSLATORS \
-			--package-name=GroupButler \
 			--package-version=4.2 \
-			--msgid-bugs-address=https://telegram.me/bac0nnn \
 			--force-po \
 			--files-from=/dev/stdin \
 			--output=/dev/stdout
